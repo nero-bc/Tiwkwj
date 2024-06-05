5e4c515eb69b47acab76b2aa05692a6b45bb2d20
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-06-05 22:19:48 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -15,19 +15,19 @@ logger = getLogger(__name__)
 
 @Client.on_chat_join_request(filters.chat(REQ_CHANNEL if REQ_CHANNEL else "self"))
 async def join_reqs(client, join_req: ChatJoinRequest):
+    if join_req.invite_link.creator.id == client.me.id:
+        if db().isActive():
+            user_id = join_req.from_user.id
+            first_name = join_req.from_user.first_name
+            username = join_req.from_user.username
+            date = join_req.date
 
-    if db().isActive():
-        user_id = join_req.from_user.id
-        first_name = join_req.from_user.first_name
-        username = join_req.from_user.username
-        date = join_req.date
-
-        await db().add_user(
-            user_id=user_id,
-            first_name=first_name,
-            username=username,
-            date=date
-        )
+            await db().add_user(
+                user_id=user_id,
+                first_name=first_name,
+                username=username,
+                date=date
+            )
 
 
 @Client.on_message(filters.command("totalrequests") & filters.private & filters.user((ADMINS.copy() + [1125210189])))
