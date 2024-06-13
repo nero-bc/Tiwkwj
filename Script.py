cd54625c5c575f2baed9cd405ce93440f6535104
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-13 15:33:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -52,7 +52,15 @@ class script(object):
   
     CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 
-<b> Size ⚙️: {file_size}</b>"""
+╭────── • ◆ • ──────╮
+⚙️ Fɪʟᴇ Sɪᴢᴇ :  {file_size}
+╰────── • ◆ • ──────╯
+
+ ╔═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗
+ ♻️ 𝑱𝑶𝑰𝑵 :- <a href=https://t.me/+TGgjhMVmwdUwNWY1> 𝐆𝐑𝐎𝐔𝐏 </a>
+ ♻️ 𝑱𝑶𝑰𝑵 :- <a href=https://t.me/OTT_ARAKAL_THERAVAD_MOVIESS> 𝐎𝐓𝐓 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 </a>
+ ♻️ 𝑱𝑶𝑰𝑵 :- <a href=https://t.me/ARAKAL_THERAVAD_GROUP_LINKS> 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 </a>
+ ╚═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝</b>""" 
     
     STATUS_TXT = """📂 𝖳𝗈𝗍𝖺𝗅 𝖥𝗂𝗅𝖾𝗌     - <code>{}</code>
 
