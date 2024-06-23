073692c89f08b3ecbd4ac6f8c5a2c4d12fb8d6fe
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-06-23 16:01:16 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -146,6 +146,7 @@ def set_global_invite(url: str):
 async def send_file(client, query, ident, file_id):
     files_ = await get_file_details(file_id)
     if not files_:
+        await query.reply("please Try again, I haved added your id to forse sub id list")
         return
     files = files_[0]
     title = files.file_name
