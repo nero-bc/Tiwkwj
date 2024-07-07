ab7382fc5a3e22e14108149afdb796e6a5750166
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-07-07 14:31:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/08e3b21c562750e6984de.
 
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '1297128957')
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1297128957').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1297128957 1509123054').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001911016310').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
