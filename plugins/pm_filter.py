49de40f00be2dc8679691ea6f9f1632556ebf105
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-10 16:26:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,6 +6,7 @@ import random
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
+from fuzzywuzzy import process
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
