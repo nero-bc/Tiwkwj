ce70e1baced7a0da87b8b21bcc6f777686ac34fc
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-10 15:48:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -224,7 +224,7 @@ async def save_group_settings(group_id, key, value):
 def get_size(size):
     """Get size in readable format"""
 
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    units = ["Bytes", "Kʙ", "Mʙ", "Gʙ", "Tʙ", "Pʙ", "Eʙ"]
     size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
